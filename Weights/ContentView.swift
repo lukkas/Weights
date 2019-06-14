f22295fcf1d4f7e21b9452fd66afbce70c477007
Lukasz Kasperek <luk.kasperek@gmail.com>
2019-06-15 00:04:51 +0200

    Initial Commit

---
+++ Weights/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Weights
+//
+//  Created by Łukasz Kasperek on 15/06/2019.
+//  Copyright © 2019 Łukasz Kasperek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ WeightsTests/WeightsTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeightsTests.swift
+//  WeightsTests
+//
+//  Created by Łukasz Kasperek on 15/06/2019.
+//  Copyright © 2019 Łukasz Kasperek. All rights reserved.
+//
+
+import XCTest
+@testable import Weights
+
+class WeightsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
