8137496ab948fd39942d21ae305d96793cf51f81
Lukasz Kasperek <luk.kasperek@gmail.com>
2022-01-30 21:25:18 +0100

    Adopt exercises subscription in exercises list vm

--- Core/Modules/ExercisesList/ExercisesListViewModel.swift
+++ Core/Modules/ExercisesList/ExercisesListViewModel.swift
@@ -6,25 +6,33 @@
 //  Copyright © 2020 Łukasz Kasperek. All rights reserved.
 //
 
+import Combine
 import Foundation
 import SwiftUI
 
 class ExercisesListViewModel: ExerciseListViewModeling {
     @Published private(set) var cellViewModels: [ExerciseCellViewModel] = []
     private let exerciseStorage: ExerciseStoring
+    private var cancellables: Set<AnyCancellable> = []
     
     init(exerciseStorage: ExerciseStoring) {
         self.exerciseStorage = exerciseStorage
-        loadContent()
+        subscribeToExercises()
     }
     
-    private func loadContent() {
-        cellViewModels = exerciseStorage.fetchExercises()
-            .map({ exercise in
-                ExerciseCellViewModel(
-                    id: exercise.id,
-                    exerciseName: exercise.name
-                )
+    private func subscribeToExercises() {
+        exerciseStorage.exercises()
+            .map { exercises in
+                exercises.map({ exercise in
+                    ExerciseCellViewModel(
+                        id: exercise.id,
+                        exerciseName: exercise.name
+                    )
+                })
+            }
+            .sink(receiveValue: { [weak self] viewModels in
+                self?.cellViewModels = viewModels
             })
+            .store(in: &cancellables)
     }
 }
