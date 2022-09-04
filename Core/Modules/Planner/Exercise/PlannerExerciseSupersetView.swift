687f57e123ffe498da00107341f94e0275d2c07b
lukkas <lukasz.kasperek@appunite.com>
2022-09-04 17:25:04 +0200

    Move exercises view to separate group

---
+++ Core/Modules/Planner/Exercise/PlannerExerciseSupersetView.swift
@@ -0,0 +1,15 @@
+//
+//  PlannerExerciseSupersetView.swift
+//  Core
+//
+//  Created by Łukasz Kasperek on 04/09/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlannerExerciseSupersetView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
